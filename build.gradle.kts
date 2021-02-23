c322f412e542bd1c47030cca4ffbab2b1dc22551
Geoffrey Challen <challen@illinois.edu>
2021-02-23 14:40:29 -0600

    Bumping version to fix Jitpack build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ allprojects {
 }
 subprojects {
     group = "com.github.cs125-illinois.jenisol"
-    version = "2021.2.6"
+    version = "2021.2.7"
     tasks.withType<JavaCompile> {
         options.compilerArgs.add("-parameters")
     }
