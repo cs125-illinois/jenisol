694674da1f6e6306346dc9714500eeb84e474843
Geoffrey Challen <challen@illinois.edu>
2021-05-10 20:19:05 -0500

    Refactor and publish.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,74 +1,103 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.File
+import java.io.StringWriter
+import java.util.Properties
+
+group = "com.github.cs125-illinois"
+version = "2021.5.1"
 
 plugins {
-    kotlin("jvm") version "1.5.0" apply false
-    id("org.jmailen.kotlinter") version "3.4.4" apply false
+    kotlin("jvm") version "1.5.0"
+    java
+    `maven-publish`
+
+    id("org.jmailen.kotlinter") version "3.4.4"
+    checkstyle
     id("com.github.sherter.google-java-format") version "0.9"
+
     id("com.github.ben-manes.versions") version "0.38.0"
     id("io.gitlab.arturbosch.detekt") version "1.16.0"
 }
-@Suppress("DEPRECATION")
-allprojects {
-    repositories {
-        mavenLocal()
-        mavenCentral()
-        jcenter()
-        maven(url = "https://jitpack.io")
+repositories {
+    mavenCentral()
+    maven(url = "https://jitpack.io")
+    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+}
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
+    implementation("io.github.classgraph:classgraph:4.8.105")
+    implementation("io.github.kostaskougios:cloning:1.10.3")
+
+    testImplementation("io.kotest:kotest-runner-junit5:4.5.0")
+    testImplementation("org.slf4j:slf4j-simple:1.7.30")
+}
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("-parameters")
+}
+tasks.withType<Test> {
+    useJUnitPlatform()
+    enableAssertions = true
+    if (JavaVersion.current() >= JavaVersion.VERSION_11) {
+        jvmArgs("-ea", "-Xmx1G", "-Xss256k", "--enable-preview")
+    } else {
+        jvmArgs("-ea", "-Xmx1G", "-Xss256k")
     }
 }
-subprojects {
-    group = "com.github.cs125-illinois.jenisol"
-    version = "2021.5.0"
-    tasks.withType<JavaCompile> {
-        options.compilerArgs.add("-parameters")
-    }
-    tasks.withType<KotlinCompile> {
-        val javaVersion = JavaVersion.VERSION_1_8.toString()
-        sourceCompatibility = javaVersion
-        targetCompatibility = javaVersion
-        kotlinOptions {
-            jvmTarget = javaVersion
-        }
-    }
-    tasks.withType<Test> {
-        useJUnitPlatform()
-        enableAssertions = true
-        if (JavaVersion.current() >= JavaVersion.VERSION_11) {
-            jvmArgs("-ea", "-Xmx1G", "-Xss256k", "--enable-preview")
-        } else {
-            jvmArgs("-ea", "-Xmx1G", "-Xss256k")
-        }
-    }
-    configurations.all {
-        resolutionStrategy {
-            force(
-                "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.0",
-                "org.jetbrains.kotlin:kotlin-script-runtime:1.5.0"
-            )
-        }
+configurations.all {
+    resolutionStrategy {
+        force(
+            "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.0",
+            "org.jetbrains.kotlin:kotlin-script-runtime:1.5.0"
+        )
     }
 }
 tasks.dependencyUpdates {
-    resolutionStrategy {
-        componentSelection {
-            all {
-                if (listOf("alpha", "beta", "rc", "cr", "m", "preview", "b", "ea", "eap", "release").any { qualifier ->
-                        candidate.version.matches(Regex("(?i).*[.-]$qualifier[.\\d-+]*"))
-                    }) {
-                    reject("Release candidate")
-                }
-            }
-        }
-    }
+    fun String.isNonStable() = !(
+        listOf("RELEASE", "FINAL", "GA").any { toUpperCase().contains(it) }
+            || "^[0-9,.v-]+(-r)?$".toRegex().matches(this)
+        )
+    rejectVersionIf { candidate.version.isNonStable() }
     gradleReleaseChannel = "current"
 }
 detekt {
-    input = files("core/src/main/kotlin")
     buildUponDefaultConfig = true
 }
-tasks.register("check") {
+tasks.check {
     dependsOn("detekt")
 }
 googleJavaFormat {
     toolVersion = "1.10.0"
+}
+tasks.compileKotlin {
+    dependsOn("createProperties")
+}
+tasks.compileTestKotlin {
+    kotlinOptions {
+        javaParameters = true
+    }
+}
+task("createProperties") {
+    dependsOn(tasks.processResources)
+    doLast {
+        val properties = Properties().also {
+            it["version"] = project.version.toString()
+        }
+        File(projectDir, "src/main/resources/edu.illinois.cs.cs125.jenisol.core.version")
+            .printWriter().use { printWriter ->
+                printWriter.print(
+                    StringWriter().also { properties.store(it, null) }.buffer.toString()
+                        .lines().drop(1).joinToString(separator = "\n").trim()
+                )
+            }
+    }
+}
+java {
+    withSourcesJar()
+}
+publishing {
+    publications {
+        create<MavenPublication>("jenisol") {
+            from(components["java"])
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,1 @@
 rootProject.name = "jenisol"
-include(":core")
