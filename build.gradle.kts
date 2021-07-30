5a04b46abae5fcc989d3a090f047c188840e8f29
Geoffrey Challen <challen@illinois.edu>
2021-07-30 16:42:52 -0500

    Bump version for next changeset.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.io.StringWriter
 import java.util.Properties
 
 group = "com.github.cs125-illinois"
-version = "2021.7.1"
+version = "2021.7.2"
 
 plugins {
     kotlin("jvm") version "1.5.21"
