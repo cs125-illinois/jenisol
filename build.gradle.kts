60fd6104c723e9cdfd90aeda4f05b75f6ad157d4
Geoffrey Challen <challen@illinois.edu>
2021-09-19 10:18:58 -0500

    Bump version for next changeset.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import java.util.Properties
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.cs125-illinois"
-version = "2021.9.1"
+version = "2021.9.2"
 
 plugins {
     kotlin("jvm") version "1.5.31"
