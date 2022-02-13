9a63990e23251113efa40aa68409cac28082af2b
Ben Nordick <ben@rigeltechnical.com>
2022-02-13 12:21:33 -0500

    Ignore synthetic methods

---
+++ src/test/java/examples/java/noreceiver/kotlinsynthetic/Incorrect0.kt
@@ -0,0 +1,15 @@
+package examples.java.noreceiver.kotlinsynthetic
+
+class Incorrect0 {
+    companion object {
+        @JvmStatic // Can't have two loose addOne Kotlin functions in the same package
+        fun addOne(value: Int): Int {
+            val transform = ::plus2
+            return transform(value)
+        }
+
+        private fun plus2(x: Int): Int {
+            return x + 2
+        }
+    }
+}
