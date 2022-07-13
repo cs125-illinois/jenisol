37995abd92a2b43f38e1ec9f7f6f807fbd70d1b7
Geoffrey Challen <challen@illinois.edu>
2022-07-13 13:33:09 -0500

    Starting linearization.

--- src/test/java/examples/java/receiver/withinitialization/Correct.java
+++ src/test/java/examples/java/receiver/withinitialization/Correct.java
@@ -1,7 +1,6 @@
 package examples.java.receiver.withinitialization;
 
 import edu.illinois.cs.cs125.jenisol.core.FixedParameters;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,6 +9,5 @@ public class Correct extends Parent {
     return value;
   }
 
-  @FixedParameters
-  private static final List<Integer> FIXED = Arrays.asList(-1, 0, 1);
+  @FixedParameters private static final List<Integer> FIXED = Arrays.asList(-1, 0, 1);
 }
