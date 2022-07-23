44c2d41d7abbbdc6dd5a7d58b3539fd9785fb8e8
Geoffrey Challen <challen@illinois.edu>
2022-07-23 16:08:39 -0500

    Bump version for next release.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import java.util.Properties
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.cs125-illinois"
-version = "2022.7.2"
+version = "2022.7.3"
 
 plugins {
     kotlin("jvm") version "1.7.10"
